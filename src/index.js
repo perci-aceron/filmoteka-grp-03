790fc4c094aeb48d0f0b53691198d1dd7c947c74
katelim215 <katelimosnero215@gmail.com>
2024-02-21 15:03:46 +0800

    partial library page functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { fetchPopularMovies } from './partials/main-page';
 import { showLoader, hideLoader } from './partials/t5-loader-functionality';
+import { showWatchedMovies, showQueueMovies } from './partials/t3-library-page';
 
 const page = 1;
 const itemsPerPage = 18;
@@ -11,3 +12,7 @@ showLoader();
 setTimeout(() => {
   hideLoader();
 }, 2000);
+
+
+showWatchedMovies();
+showQueueMovies();
